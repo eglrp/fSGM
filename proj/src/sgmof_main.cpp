5f0815aff4e20406fedbed0d36a1a8d57c5750e6
Megamusz <zhangdongzju@163.com>
2018-02-04 21:44:31 +0800

    add calibration option in main entry

--- proj/src/sgmof_main.cpp
+++ proj/src/sgmof_main.cpp
@@ -4,6 +4,7 @@
 #include "sgmof_main.h"
 #include "epi_sgm.h"
 #include "pyd_sgm.h"
+#include "utils.h"
 
 using namespace cv;
 
@@ -13,6 +14,8 @@ const String keys =
     "{@I2 image2     |<none>| second image   }"
     "{o outFile      |flow.png| output flow file (in KITTI format)}"
     "{m mode         |0     | epiSGM(0)/pydSGM mode(1) }"
+	"{c calibFile    |calib.txt| calibration file, must have when mode = 0}"
+	"{b benchmark    |0     | 0/1 for specifying Kitti2012/kitti2015 benchmark, used in EpiSGM only (calibration file has different format for 2012/2015)}"
     "{p passNum      |2     | number of SGM passes   }"
     "{d enableDiagonal |    | enable diagnoal directions in SGM }"
     "{V vzIndex      |      | enable vz-index in epipolar SGM    }"
@@ -34,6 +37,8 @@ int main(int argc, char** argv )
     String image1FileName = parser.get<String>("image1");
     String image2FileName = parser.get<String>("image2");
     int mode = parser.get<int>("mode");
+	String calibFileName = parser.get<String>("calibFile");
+	int benchmark = parser.get<int>("benchmark");
 
     if (!parser.check())
     {
@@ -41,7 +46,7 @@ int main(int argc, char** argv )
         exit(1);
     }
 
-    Mat I1, I2;
+    Mat I1, I2, flow;
     I1 = imread(image1FileName, IMREAD_UNCHANGED);
     I2 = imread(image2FileName, IMREAD_UNCHANGED);
     if (I1.data == NULL || I2.data == NULL) {
@@ -51,10 +56,13 @@ int main(int argc, char** argv )
     else if (I1.size != I2.size) {
         std::cout << "Size of image1/2 must match" << std::endl;
     }
-   
-    Mat flow;
     
     if (mode == 0) {
+		//check if calibration file provided
+		Mat P = read_calib_file(calibFileName, benchmark == 1);
+		//get intrinsic matrix
+		Mat K = P(Range(0, 3), Range(0, 3));
+
         //call EpiSGM OF to calculate optical flow
         EpiSGM epiSGM;
         flow = epiSGM.compute(I1, I2);
